9425691b42f4b4f6d15cdaf9c94adb724336ae7a
Jean Zonta <jeanzonta777@hotmail.com.br>
2022-12-25 17:00:14 -0300

    testando o programa

--- lib/main.dart
+++ lib/main.dart
@@ -6,21 +6,24 @@ import 'package:flutter/material.dart';
 //https://api.giphy.com/v1/gifs/search?api_key=f7ToaKTLTCKGcdMevenFOg5aN1r0hmqq&q=dogs&limit=25&offset=75&rating=g&lang=pt
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: const HomePage(),
-    theme: ThemeData(
-      //Deixando a borda da pesquisa branca
-      inputDecorationTheme: const InputDecorationTheme(
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+      theme: ThemeData(
+        //Deixando a borda da pesquisa branca
+        inputDecorationTheme: const InputDecorationTheme(
           //quando inativa fica braca
           enabledBorder: OutlineInputBorder(
             borderSide: BorderSide(color: Colors.white),
           ),
 
           //quando ativa fica branca
-          focusedBorder:
-              OutlineInputBorder(borderSide: BorderSide(color: Colors.white))),
-      ),
-    )//MaterialApp
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.white),
+          ), //OutlineInputBorder
+        ), //InputDecorationTheme
+      ), //ThemeData
+    ), //MaterialApp
   );
 }
